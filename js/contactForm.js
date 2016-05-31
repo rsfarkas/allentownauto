44e3a38d5d0112c5372284a2ae02dcacf1eb6987
rsfarkas <roxannesfarkas@gmail.com>
2016-05-30 22:39:11 -0400

    added spacing to js

--- js/contactForm.js
+++ js/contactForm.js
@@ -19,9 +19,9 @@
 
       var c_name = $('#name').val();
       var c_email = $('#email').val();
-      var c_message = $('#message').val();
-      var c_phone = $('#phone').val();
-      var c_company = $('#company').val();
+      var c_message = $('#message ').val();
+      var c_phone = $('#phone ').val();
+      var c_company = $('#company ').val();
       var responseMessage = $('#contactForm .ajax-response');
 
       if (( c_name== '' || c_email == '' || c_message == '') || (!isValidEmailAddress(c_email) )) {
